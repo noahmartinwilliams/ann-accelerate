cd6cbc46aaa276b700d7520cad092da5dc91ffdf
Noah Williams <noahmartinwilliams@gmail.com>
2023-11-01 16:29:32 -0500

    Add ML.ANN.Layer module with Layer data type

--- src/ML/ANN/ActFuncs.hs
+++ src/ML/ANN/ActFuncs.hs
@@ -1,7 +1,9 @@
-module ML.ANN.ActFuncs (sigmoid) where
+module ML.ANN.ActFuncs (sigmoid, ActFunc(..)) where
 
 import Data.Array.Accelerate as A
 import Prelude as P
+
+data ActFunc = Sigmoid Int deriving(Show, Eq)
 
 sigmoid :: Exp Double -> Exp Double
 sigmoid x = let one = constant 1.0 in one / (one + (exp (-x)))
---
+++ src/ML/ANN/Layer.hs
@@ -0,0 +1,11 @@
+module ML.ANN.Layer (Layer(..), LSpec(..)) where
+
+import Data.Array.Accelerate as A
+import Prelude as P
+
+import ML.ANN.Mat 
+import ML.ANN.Vect
+
+type LSpec = [ActFunc]
+
+data Layer = SGDLayer (Mat OutputSize InputSize) (Vect OutputSize) LSpec
