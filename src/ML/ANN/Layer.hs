adb76b59d56eb7cce126d0af0426c0fe421efe65
Noah Williams <noahmartinwilliams@gmail.com>
2023-11-08 17:04:13 -0600

    Add function for generating networks
    The current version of mkNetwork uses the same lazily generated list of random numbers for every layer, that will need to be fixed later.

--- src/ML/ANN/Layer.hs
+++ src/ML/ANN/Layer.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE GADTs, DataKinds, KindSignatures, TypeOperators #-}
-module ML.ANN.Layer (Layer(..), LSpec(), calcLayer, lspecGetNumOutputs) where
+module ML.ANN.Layer (Layer(..), LSpec(), calcLayer, lspecGetNumOutputs, layerGetNumInputs, mkSGDLayer) where
 
 import Data.Array.Accelerate as A
 import Prelude as P
@@ -12,6 +12,12 @@ type LSpec = [ActFunc]
 
 data Layer = SGDLayer Int (Mat OutputSize InputSize) (Vect OutputSize) LSpec deriving (Show) -- NumInputs weights bias lspec
 
+mkSGDLayer :: [Double] -> LSpec -> Int -> Int -> Layer
+mkSGDLayer randoms lspec numInputs numOutputs = do
+    let weightsM = use (fromList (Z:.numOutputs:.numInputs) randoms)
+        biasesM = use (fromList (Z:.numOutputs:.1) randoms)
+    SGDLayer numInputs (MatOI weightsM) (VectO biasesM) lspec
+
 calcLayer :: Layer -> Acc (Matrix Double) -> Acc (Matrix Double)
 calcLayer (SGDLayer _ weights bias lspec) x = do
     let x2 = VectI x
@@ -21,3 +27,6 @@ calcLayer (SGDLayer _ weights bias lspec) x = do
 lspecGetNumOutputs :: LSpec -> Int
 lspecGetNumOutputs [] = 0
 lspecGetNumOutputs ((Sigmoid x) : rest) = x + (lspecGetNumOutputs rest)
+
+layerGetNumInputs :: Layer -> Int
+layerGetNumInputs (SGDLayer num _ _ _ ) = num
