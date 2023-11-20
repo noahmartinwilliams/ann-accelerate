b9c847d1d5a653bf4216e6de485516622aee2e26
Noah Williams <noahmartinwilliams@gmail.com>
2023-11-20 16:28:42 -0600

    Add trainNet command for training neural networks from files

--- src/ML/ANN/File.hs
+++ src/ML/ANN/File.hs
@@ -1,4 +1,4 @@
-module ML.ANN.File(block2bs) where
+module ML.ANN.File(block2bs, bs2block) where
 
 import ML.ANN.Block
 import ML.ANN.Mat
@@ -16,8 +16,24 @@ import Data.Serialize
 
 block2bs :: (BlockInfo, BlockV) -> BL.ByteString
 block2bs (blinfo, (vecti, vectd)) = do
-    let ver = BL.pack [0, 1, 0]
-        blinfobs = fromStrict (encode blinfo)
-        vectibs = fromStrict (A.toByteStrings vecti)
-        vectdbs = fromStrict (A.toByteStrings vectd)
-    ver <> blinfobs <> vectibs <> vectdbs 
+    let ver = BL.pack [127, 65, 78, 78, 0, 0, 1, 0]
+        vectil = A.toList vecti
+        vectdl = A.toList vectd
+    ver <> (fromStrict (encode (blinfo, vectil, vectdl)))
+
+bs2block :: MonadFail m => BL.ByteString -> m (BlockInfo, BlockV)
+bs2block bs = do
+    let (_: 65: 78: 78: 0: major: minor: bug:_) = BL.unpack bs
+    (blinfo, vecti, vectd) <- getInfo major minor bug (BL.drop 8 bs)
+    return (blinfo, (vecti, vectd))
+
+getInfo :: MonadFail m => Word8 -> Word8 -> Word8 -> BL.ByteString -> m (BlockInfo, Vector Int, Vector Double)
+getInfo 0 1 0 bs = do
+    (blinfo, vectil, vectdl) <- intern (decode (toStrict bs))
+    let vecti = A.fromList (Z:.(P.length vectil)) vectil
+        vectd = A.fromList (Z:.(P.length vectdl)) vectdl
+    return (blinfo, vecti, vectd) where
+        intern :: MonadFail m => Either String (BlockInfo, [Int], [Double]) -> m (BlockInfo, [Int], [Double])
+        intern (Left str) = fail str
+        intern (Right a) = return a
+getInfo major m b _ = error ("Version " P.++ (show major) P.++ "." P.++ (show m) P.++ "." P.++ (show b) P.++ " is not supported.")
---
+++ trainNet/Main.hs
@@ -0,0 +1,59 @@
+module Main where
+
+import Prelude as P
+import Data.Array.Accelerate as A
+import Data.Array.Accelerate.LLVM.PTX
+import System.Console.GetOpt
+import System.Environment
+import System.IO
+import Data.Maybe
+import ML.ANN
+import Data.ByteString.Lazy as B
+import Data.List.Split
+import Text.Printf
+
+data Options = Options { optNet :: String, optInput :: String, optOutput :: String, optSave :: String }
+
+startOptions :: Options
+startOptions = Options { optNet = "network.ann", optInput = "input.txt", optOutput = "output.txt", optSave = "network-trained.ann" }
+
+options :: [OptDescr (Options -> IO Options)]
+options = [ Option "I" ["input-net"] (ReqArg (\arg -> \opt -> return opt { optNet = arg }) "network.ann") "input neural net file.",
+            Option "i" ["input-data"] (ReqArg (\arg -> \opt -> return opt { optInput = arg }) "input.txt") "input data file.",
+            Option "o" ["output-data"] (ReqArg (\arg -> \opt -> return opt { optOutput = arg }) "output.txt") "output data file.",
+            Option "O" ["output-net"] (ReqArg (\arg -> \opt -> return opt { optSave = arg }) "network-trained.ann") "output neural net file." ]
+
+
+train :: (Vector Int, Vector Double) -> [(Vector Double, Vector Double)] -> ((Vector Int, Vector Double) -> (Vector Double, Vector Double) -> (Vector Double, Vector Int, Vector Double)) -> ([Vector Double], (Vector Int, Vector Double))
+train block [] _ = ([], block)
+train block ( head : rest ) fn = do
+    let (err, ints, doubles) = fn block head 
+        (rest2, block2) = train (ints, doubles) rest fn
+    (err : rest2, block2)
+
+getSamplesLL :: [String] -> [([Double], [Double])]
+getSamplesLL inplist = do
+    let (inputs, outputs) = P.unzip (P.map (\x -> ((endBy "#" x) P.!! 0, (endBy "#" x) P.!! 1)) inplist)
+        inputsl = P.map (\x -> endBy "," x) inputs
+        outputsl = P.map (\x -> endBy "," x) outputs
+        inputsld = P.map (\x -> P.map (\y -> read y :: Double) x) inputsl
+        outputsld = P.map (\x -> P.map (\y -> read y :: Double) x) outputsl
+    P.zip inputsld outputsld
+
+main :: IO ()
+main = do
+    args <- getArgs
+    let (actions, _, errors) = getOpt RequireOrder options args
+    opts <- P.foldl (>>=) (return startOptions) actions
+    let Options { optNet = netfilename, optInput = inputfilename, optOutput = outputfilename, optSave = outnetfilename} = opts
+    annInBS <- B.readFile netfilename 
+    input <- System.IO.getContents
+    let inplines = endBy "\n" input
+    (blinfo, blockv) <- bs2block annInBS
+    let fn = runN (\x -> \y -> trainOnceLinReg (ANN blinfo x) y)
+        samplesll = getSamplesLL inplines
+        samplesvv = P.map (\(x, y) -> ((A.fromList (Z:.(P.length x)) x :: Vector Double), (A.fromList (Z:.(P.length y)) y :: Vector Double))) samplesll
+        (errors, blockv2) = train blockv samplesvv fn 
+        errorsStr = P.map (\x -> (printf "%.5F" ((toList x) P.!! 0) )  P.++ "\n") errors
+    System.IO.putStr (P.foldr (P.++) "" errorsStr)
+    B.writeFile outnetfilename (block2bs (blinfo, blockv2))
