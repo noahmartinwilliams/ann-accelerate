aba9334989d4e7bc0ddc95103d42e4a5aad55c9d
Noah Williams <noahmartinwilliams@gmail.com>
2026-03-26 19:52:30 -0400

    Add Batch Backpropagation Of Layers

--- src/ML/ANN/BPLayer.hs
+++ src/ML/ANN/BPLayer.hs
@@ -8,6 +8,24 @@ import ML.ANN.MkLayer
 import ML.ANN.Types
 import Prelude as P
 
+batchBPLayers :: [LLayer] -> Optim -> [AccMat Double Outp One] -> (Layer, AccMat Double Outp One)
+batchBPLayers llayers (SGDOptim lr) bpLayers = do
+    let (layers, (fbps : bps)) = P.unzip (P.zipWith (\x -> \bp -> bpLayer x (SGDOptim lr) bp) llayers bpLayers)
+        ws = P.map lweights layers
+        bs = P.map lbiases layers
+        numLayers = P.length llayers
+        numLayersD = constant (1.0 / (P.fromIntegral numLayers :: Double))
+        (firstWs : wsRest) = ws
+        (firstBs : bsRest) = bs
+        ws' = avgMs firstWs wsRest numLayersD
+        bs' = avgMs firstBs bsRest numLayersD
+        firstLayer = layers P.!! 0
+    (firstLayer { lweights = ws', lbiases = bs'}, avgMs fbps bps numLayersD) where
+
+        avgMs :: AccMat Double a b -> [AccMat Double a b] -> Exp Double -> AccMat Double a b
+        avgMs m l d = d `matScale` (P.foldl matAdd m l )
+
+
 bpLayer :: LLayer -> Optim -> AccMat Double Outp One -> (Layer, AccMat Double Outp One)
 bpLayer (LLayer { llprevInput = prevInput, llayer = l@(Layer { lnumInputs = ni, lweights = w, lbiases = b, llspec = lspec})}) (SGDOptim lr) bp = do
     let wT = matTransp w
@@ -21,3 +39,12 @@ bpLayer (LLayer { llprevInput = prevInput, llayer = l@(Layer { lnumInputs = ni, 
         b' = b `matSub` (lr `matScale` db)
         (AccMat bp'' Inp One) = wT `matMul` bp
     (l { lweights = w', lbiases = b'}, AccMat bp'' Outp One)
+bpLayer (LLayer { llprevInput = prev, llayer = layer@(InpLayer { vweights = w, vbiases = b, vlspec = lspec })}) (SGDOptim lr) bp = do
+    let (AccMat prev' Inp One) = prev
+        prev'' = AccMat (A.transpose prev') One Outp
+        prev2 = matTransp prev''
+        deriv = matTransp (dactFuncs lspec prev'')
+        w' = w `matSub` (lr `matScale` (deriv `matZipMul` (bp `matZipMul` prev2)))
+        b' = b `matSub` (lr `matScale` (deriv `matZipMul` bp))
+        bp' = w `matZipMul` (deriv `matZipMul` bp)
+    ( layer { vweights = w', vbiases = b' }, bp')
--- src/ML/ANN/LLayer.hs
+++ src/ML/ANN/LLayer.hs
@@ -8,5 +8,8 @@ import Prelude as P
 
 learnLayer :: Layer -> Acc (Matrix Double) -> (LLayer, Acc (Matrix Double))
 learnLayer layer inp = do
-    let inferred = inferLayer inp layer
+    let inferred = inferLayer layer inp
     (LLayer { llprevInput = (AccMat inp Inp One), llayer = layer }, inferred)
+
+batchLearnLayers :: Layer -> [Acc (Matrix Double)] -> ([LLayer], [Acc (Matrix Double)])
+batchLearnLayers l inpLs = P.unzip (P.map (learnLayer l) inpLs)
